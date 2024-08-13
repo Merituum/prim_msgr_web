056ad09451067ce680208c4a47d427b3e141435f
Merituum <bartolopoland@gmail.com>
2024-08-13 19:47:41 +0200

    adjusted main page - beggining of chat

--- main.php
+++ main.php
@@ -29,8 +29,19 @@
             </tr>
         </table>
     </div>
+ 
     <?php
     session_start();
+
+    $db_name = "localhost";
+    $db_user = "root";
+    $db_pass = "";    
+    $db_name_db = "prim_msgr";
+    $conn = new mysqli($db_name, $db_user, $db_pass, $db_name_db);
+    dashboard_display_friends($conn);
+    if ($conn->connect_error) {
+        die("Nie można połączyć się z bazą danych: " . $conn->connect_error);
+    }
     if (isset($_POST["dodaj_znajomego"])) {
         header("Location: index.php");
         exit();
@@ -40,6 +51,57 @@
         header("Location: login.php");
         exit();
     }
+    function dashboard_display_friends($conn) {
+        $user_login=$_SESSION['login'];
+        $querry_id="SELECT id FROM users WHERE login='$user_login'";
+        $result_id = $conn->query($querry_id);
+        $row = $result_id->fetch_assoc();
+        $user_id=$row['id'];
+
+        $querry_friends="SELECT ID_user_friends FROM friendship WHERE ID_user='$user_id'";
+        $result_friends_IDs=$conn->query($querry_friends);
+
+       
+    if ($result_friends_IDs->num_rows > 0) {
+        echo "<div>";
+        echo "<table id='kafelki' class='user-table'>";
+        echo "<tr>";
+        // echo "<th>Imie</th>";
+        // echo "<th>Nazwisko</th>";
+        echo "<th>Login</th>";
+        echo "<th>Usuń znajomego</th>";
+        echo "</tr>";
+
+        while ($row_friends_IDs = $result_friends_IDs->fetch_assoc()) {
+            $friend_id = $row_friends_IDs['ID_user_friends'];
+            $querry_friend = "SELECT * FROM users WHERE id='$friend_id'";
+            $result_friend = $conn->query($querry_friend);
+            $row_friend = $result_friend->fetch_assoc();
+
+            echo "<tr>";
+            // echo "<td>".$row_friend['name']."</td>";
+            // echo "<td>".$row_friend['surname']."</td>";
+            echo "<td>".$row_friend['Login']."</td>";
+            echo "<td><form method='post' action=''><input type='submit' value='Usuń' name='usun_znajomego'></form></td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Brak znajomych do wyświetlenia.";
+    }
+    echo "</div>";
+    if (isset($_POST["usun_znajomego"])) {
+        
+        $user_id=$_SESSION['id'];
+        $querry_delete_friend="DELETE FROM friendship WHERE ID_user='$user_id' AND ID_user_friends='$friend_id'";
+        $conn->query($querry_delete_friend);
+        header("Location: main.php");
+        
+    }
+
+    }
+    
 ?>
 </body>
 </html>
